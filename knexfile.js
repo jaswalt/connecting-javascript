a2dd98744d32c06bf4d281d6406e07b52eeacaf0
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-24 20:55:00 +0000

    initial commit

---
+++ knexfile.js
@@ -0,0 +1,51 @@
+const pg = require("pg");
+const settings = require("./settings");
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      filename: './dev.pg',
+      user     : settings.user,
+      password : settings.password,
+      database : settings.database,
+      host     : settings.hostname,
+      port     : settings.port,
+      ssl      : settings.ssl
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
